5c7865753a6b2125a92508fb40dd9ae45b11c7cb
Kaua <kauakirchner2@gmail.com>
2022-05-18 20:46:09 -0300

    Instalando o vue-gravatar

--- DevInventary/projeto-dev-inventary/src/main.js
+++ DevInventary/projeto-dev-inventary/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
+import VueGravatar from 'vue3-gravatar'
 
 
 const app = createApp(App);
 app.use(router)
+app.use(VueGravatar)
 app.mount('#app');
