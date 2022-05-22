0be1c6c80a499e23583be6b7fcf21d35fa4be538
Kaua <kauakirchner2@gmail.com>
2022-05-22 09:30:08 -0300

    Layout feito.

--- DevInventary/projeto-dev-inventary/src/Views/TelaCadastro.vue
+++ DevInventary/projeto-dev-inventary/src/Views/TelaCadastro.vue
@@ -1,44 +1,61 @@
 <template>
-  <div class="container">
-    <router-view></router-view>
-    <h1>Preencha o formulário para se cadastrar</h1>
-    <form>
-        <div class="form">
-            <h2 class="my-4">Preencha o formulário para se cadastrar</h2>
-            <div class="row">
-                <div class="col-2">
-                    <label class="label-control" name="email">Email</label>
-                    <input type="email" name="email" class="form-control" v-model="usuario.email"/>
-                    <span class="text-danger" v-text="errors.email" v-show="errors.email"></span>
+    <div class="container-fluid bg-white">
+        <div class="row justify-content-md-center">
+            <div class="col-5">
+                <div class="col-12 mt-5">
+                    <section>
+                        <h4 class="text-primary">Cadastre-se</h4>
+                        <vee-form id="form-cadastro-colaboradores" @submit="save" :validation-schema="schema" v-slot="{ errors }">
+                            <div class="form-group mt-5">
+                                <label class="label-control text-secondary">Email</label>
+                                <vee-field type="email" name="email" class="form-control" v-model="usuario.email"/>
+                                <span class="text-danger" v-text="errors.email" v-show="errors.email"></span>
+                            </div>
+                            <div class="form-group">
+                                <label class="label-control text-secondary">Senha</label>
+                                <vee-field type="password" name="senha" class="form-control" v-model="usuario.senha"/>
+                                <span class="text-danger" v-text="errors.senha" v-show="errors.senha"></span>
+                            </div>
+                            <div class="form-group">
+                                <label class="label-control text-secondary">Confirme sua senha</label>
+                                <vee-field type="password" name="senhaConfirmada" class="form-control" v-model="usuario.senhaConfirmada"/>
+                                <span class="text-danger" v-text="errors.senhaConfirmada" v-show="errors.senhaConfirmada"></span>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-outline-primary mt-3">Cadastrar</button>
+                                </div>
+                            </div>
+                        </vee-form>   
+                    </section>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-2">
-                    <label class="label-control">Senha</label>
-                    <input type="password" class="form-control" v-model="usuario.senha">
-                </div>    
-            </div>
-            <div class="row">
-                <div class="col-2">
-                    <label class="label-control">Confirmar Senha</label>
-                    <input type="password" class="form-control" v-model="usuario.senhaConfirmada">
-                </div>   
+            <div class="col-7">
+                <div class="col-12">
+                    <img src="../assets/imgCadastro.jpg" alt="" class="img-fluid">
+                </div>
             </div>
         </div>
-        <div class="col-2 my-3">
-            <button type="submit" class="btn btn-success">Enviar</button>
-        </div>
-    </form>
-    <div class="row">
-       
     </div>
-  </div>
 </template>
 
 <script>
+import {Form, Field} from 'vee-validate'
+import  '../Validate'
 export default {
+    components:{
+        'vee-form': Form,
+        'vee-field': Field
+    },
     data(){
+        const schema = {
+            email: 'required',
+            senha: 'required',
+            senhaConfirmada: 'required'
+        }
+
         return{
+            schema,
             usuarios:[],
             usuario:{
                 email: null,
@@ -52,7 +69,7 @@ export default {
         save(){
             console.log('caiu')
             this.$store.commit('autenticacaoModule/cadastrarNovoUsuario', this.usuario)
-            this.$router.push('/login')
+            // this.$router.push('/login')
             console.log(this.usuario.email)
           
            
