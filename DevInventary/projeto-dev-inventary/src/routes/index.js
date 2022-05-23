1b060b90ac2bb458ba697fc55a3b17167d973484
Kaua <kauakirchner2@gmail.com>
2022-05-23 20:01:19 -0300

    Layout em andamento.

--- DevInventary/projeto-dev-inventary/src/routes/index.js
+++ DevInventary/projeto-dev-inventary/src/routes/index.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import TelaLogin from '../Views/TelaLogin.vue'
 import TelaCadastro from '../Views/TelaCadastro.vue'
+import CadastroColaboradores from '../Views/CadastroColaboradores.vue'
 
 const routes = [
     {path: '/login', component: TelaLogin},
-    {path: '/cadastro', component: TelaCadastro}
+    {path: '/cadastro', component: TelaCadastro},
+    {path: '/cadastroColaboradores', component: CadastroColaboradores}
 ]
 
 const router = createRouter({
