ffc6c7b4fefd389a8688b807bbb0251830d4dc2b
Kaua <kauakirchner2@gmail.com>
2022-05-25 14:34:42 -0300

    Esqueleto feito

---
+++ DevInventary/projeto-dev-inventary/src/Views/ListagemColaborador.vue
@@ -0,0 +1,39 @@
+<template>
+    <router-view></router-view>
+  <div class="container-fluid">
+      <div class="row justify-content-md-center">
+          <div class="col-12">
+              <h4 class="text-primary main-title">Digite o nome do colaborador para buscar</h4>
+              <div class="row-mt-2">
+                  <div class="col-3">
+                      <input type="text" class="form-control form-control-sm" placeholder="digite o nome do colaborador"> 
+                  </div>
+              </div>
+                <div class="col mt-2">
+                    <button class="btn btn-outline-primary">Buscar</button>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card">
+                       <div class="card-body">
+                            <!--Img gravatar-->
+                            <h5 class="card-title">Nome da pessoa</h5>
+                            <span class="card-text text-secondary">Email</span>
+                            <p  class="card-text text-secondary">Numero</p>
+                            <h6  class="card-title">Cargo</h6>
+                        </div>
+                    </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- DevInventary/projeto-dev-inventary/src/routes/index.js
+++ DevInventary/projeto-dev-inventary/src/routes/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import TelaLogin from '../Views/TelaLogin.vue'
 import TelaCadastro from '../Views/TelaCadastro.vue'
 import CadastroColaboradores from '../Views/CadastroColaboradores.vue'
+import ListagemColaborador from '../Views/ListagemColaborador.vue'
 
 const routes = [
     {path: '/login', component: TelaLogin},
     {path: '/cadastro', component: TelaCadastro},
-    {path: '/cadastroColaboradores', component: CadastroColaboradores}
+    {path: '/cadastroColaboradores', component: CadastroColaboradores},
+    {path: '/listaColaboradores', component: ListagemColaborador}  
 ]
 
 const router = createRouter({
