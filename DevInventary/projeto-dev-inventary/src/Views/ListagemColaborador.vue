868a5c62183bb6d3904709a62ac4d8ffecdb685f
Kaua <kauakirchner2@gmail.com>
2022-05-26 15:20:35 -0300

    layout feito

--- DevInventary/projeto-dev-inventary/src/Views/ListagemColaborador.vue
+++ DevInventary/projeto-dev-inventary/src/Views/ListagemColaborador.vue
@@ -5,22 +5,25 @@
           <div class="col-12">
               <h4 class="text-primary main-title">Digite o nome do colaborador para buscar</h4>
               <div class="row-mt-2">
-                  <div class="col-3">
-                      <input type="text" class="form-control form-control-sm" placeholder="digite o nome do colaborador"> 
+                  <div class="col-1">
+                      <input type="text" class="form-control form-control-sm" id="inptBusca" placeholder="digite o nome do colaborador"> 
                   </div>
+                   <div class="col-2 mt-2">
+                        <button class="btn btn-outline-primary">Buscar</button>
+                    </div>
               </div>
-                <div class="col mt-2">
-                    <button class="btn btn-outline-primary">Buscar</button>
-                </div>
                 <div class="col-md-3 mt-3">
-                    <div class="card">
-                       <div class="card-body">
-                            <!--Img gravatar-->
+                    <div class="card" v-if="mostraInfos"> 
+                       <div class="card-body" @click="mostraInfos = !mostraInfos">
+                            <vue-gravatar class="gravatar"></vue-gravatar>
                             <h5 class="card-title">Nome da pessoa</h5>
                             <span class="card-text text-secondary">Email</span>
                             <p  class="card-text text-secondary">Numero</p>
                             <h6  class="card-title">Cargo</h6>
                         </div>
+                    </div>
+                    <div v-else>
+                        todas as infos do colaborador....
                     </div>
                 </div>
           </div>
@@ -30,10 +33,24 @@
 
 <script>
 export default {
-
+   data(){
+       return{
+           mostraInfos: true
+       }
+   },
+   methods:{
+       
+   }
 }
 </script>
 
 <style>
-
+.gravatar{
+    width: 4vw;
+    height: 8vh;
+}
+#inptBusca{
+    width: 16vw;
+    height: 5vh;
+}
 </style>
