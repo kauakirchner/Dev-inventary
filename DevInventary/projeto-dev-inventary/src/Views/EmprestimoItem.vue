56b6b161ea07c85ef3afc63e3344fd30ca8bddfd
Kaua <kauakirchner2@gmail.com>
2022-05-29 16:46:14 -0300

    Tela de emprestimos feita.

---
+++ DevInventary/projeto-dev-inventary/src/Views/EmprestimoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+      <div class="container mt-3">
+          <div class="content input-group">
+              <input type="text" class="w-75 form-control">
+              <select class="badge bg-primary text-white text-center">
+                  <option>Código de Patrimonio</option>
+                    <option>
+                      Pelo título
+                    </option>
+                    <option>
+                        Pela Categoria</option>
+                    <option>
+                        Pelo Colaborador
+                    </option>
+                </select>
+            </div>
+            <hr>
+            <h4>Lista de Itens</h4>
+            <p class="text-danger">
+                Não há itens cadastrados - 
+                <router-link to="/cadastroProduto" class="btn btn-outline-danger">
+                Realizar novo cadastro
+                </router-link>
+            </p>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- DevInventary/projeto-dev-inventary/src/routes/index.js
+++ DevInventary/projeto-dev-inventary/src/routes/index.js
@@ -4,7 +4,8 @@ import TelaCadastro from '../Views/TelaCadastro.vue'
 import CadastroColaboradores from '../Views/CadastroColaboradores.vue'
 import ListagemColaborador from '../Views/ListagemColaborador.vue'
 import CadastroProduto from '../Views/CadastroProduto.vue'
-import InventarioA from '../Views/InventarioA.vue'
+import InventarioA from '../Views/InventarioA.vue'//Esse A no final é por conta que o vue não estava deixando eu criar o componente só com o nome inventário
+import EmprestimoItem from '../Views/EmprestimoItem.vue'
 
 const routes = [
     {path: '/', component: TelaLogin},
@@ -12,7 +13,8 @@ const routes = [
     {path: '/cadastroColaboradores', component: CadastroColaboradores},
     {path: '/listaColaboradores', component: ListagemColaborador},
     {path: '/CadastroProduto', component: CadastroProduto},
-    {path: '/inventario', component: InventarioA}   
+    {path: '/inventario', component: InventarioA},
+    {path: '/emprestimoItem', component: EmprestimoItem}   
 ]
 
 const router = createRouter({
