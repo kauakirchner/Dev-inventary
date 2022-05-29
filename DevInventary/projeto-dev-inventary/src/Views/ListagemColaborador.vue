86d907f8b374fe4fcaf42a8638c7b5a3927dca16
Kaua <kauakirchner2@gmail.com>
2022-05-29 13:32:05 -0300

    Cards alinhados lado a lado.

--- DevInventary/projeto-dev-inventary/src/Views/ListagemColaborador.vue
+++ DevInventary/projeto-dev-inventary/src/Views/ListagemColaborador.vue
@@ -3,29 +3,28 @@
   <div class="container-fluid">
       <div class="row justify-content-md-center">
           <div class="col-12">
-              <h4 class="text-primary main-title">Digite o nome do colaborador para buscar</h4>
-              <div class="row-mt-2">
-                  <div class="col-1">
-                      <input type="text" class="form-control form-control-sm" id="inptBusca" placeholder="digite o nome do colaborador"> 
-                  </div>
-                   <div class="col-2 mt-2">
-                        <button class="btn btn-outline-primary">Buscar</button>
-                    </div>
-              </div>
-                <div class="col-md-3 mt-3">
-                    <div class="card" v-for="colaborador in colaboradores" :key="colaborador.id"> 
-                       <div class="card-body">
-                            <vue-gravatar class="gravatar" email="{{colaborador.email}}"></vue-gravatar>
-                            <h5 class="card-title">{{colaborador.nome}}</h5>
-                            <span class="card-text text-secondary">{{colaborador.email}}</span>
-                            <p  class="card-text text-secondary">{{colaborador.telefone}}</p>
-                            <h6  class="card-title">{{colaborador.cargo}}</h6>
+            <div class="row g-3 mt-3">
+                    <div class="">
+                        <h4 class="text-primary main-title">Digite o nome do colaborador para buscar</h4>
+                            <div class="row-mt-2">
+                                <input type="text" class="form-control form-control" id="inptBusca" placeholder="digite o nome do colaborador">
+                                <div class="col-sm-12 col-md-6 col-lg-3 mt-2">
+                                    <button class="btn btn-outline-primary">Buscar</button> 
+                                </div>
+                            </div>
+                        <div class="row">
+                            <div class="card col-sm-12 col-md-6 col-lg-3 mt-3" v-for="colaborador in colaboradores" :key="colaborador.id">
+                                <div class="card-body">
+                                <vue-gravatar class="gravatar" email="{{colaborador.email}}"></vue-gravatar>
+                                <h5 class="card-title">{{colaborador.nome}}</h5>
+                                <span class="card-text text-secondary">{{colaborador.email}}</span>
+                                <p  class="card-text text-secondary">{{colaborador.telefone}}</p>
+                                <h6  class="card-title">{{colaborador.cargo}}</h6>
+                            </div>
                         </div>
                     </div>
-                    <!-- <div v-else>
-                        todas as infos do colaborador....
-                    </div> -->
                 </div>
+            </div>
           </div>
       </div>
   </div>
