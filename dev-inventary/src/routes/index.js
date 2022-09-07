e3adc401d7ebaf48963141bdd3856350def6216e
Kaua <kauakirchner2@gmail.com>
2022-09-07 14:15:26 -0300

    Refactory Colaborators Register styles

--- dev-inventary/src/routes/index.js
+++ dev-inventary/src/routes/index.js
@@ -1,16 +1,16 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import TelaLogin from '../Views/TelaLogin.vue'
 import TelaCadastro from '../Views/TelaCadastro.vue'
-import CadastroColaboradores from '../Views/CadastroColaboradores.vue'
 import ListagemColaborador from '../Views/ListagemColaborador.vue'
 import CadastroProduto from '../Views/CadastroProduto.vue'
 import InventarioA from '../Views/InventarioA.vue'
 import EmprestimoItem from '../Views/EmprestimoItem.vue'
+import ColaboratorsRegister from '../Views/ColaboratorsRegister.vue'
 
 const routes = [
     {path: '/', component: TelaLogin},
     {path: '/cadastrar-usuario', component: TelaCadastro},
-    {path: '/cadastrar-colaboradores', component: CadastroColaboradores,
+    {path: '/cadastrar-colaboradores', component: ColaboratorsRegister,
         beforeEnter: (to) =>{
             const autenticado = localStorage.getItem('loginUsuario')
             if(autenticado){
